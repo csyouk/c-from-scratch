40b439b0ec7e46cd877bd262133e229f93846795
csyouk <csyouk@hanmail.net>
2017-07-21 08:44:06 +0900

    add exercises

---
+++ exam.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 10;
+
+	switch ( 10 % 3 )
+	{
+
+		//case 3: a++;
+		//case 2: a++; break;
+		//default: a++;                --> 11
+
+		//default: a++;
+		//case 3: a++;
+		//case 2: a++; break;          --> 13
+
+		//case 3: a++;
+		//default: a++;
+		//case 2: a++; break;          --> 12  
+
+
+	}
+
+	printf("%d", a);  
+}
